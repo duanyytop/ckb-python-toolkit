d1b8a63446c747a19959d5423179cd08b9bb5c71
Dylan <duanyytop@gmail.com>
2021-07-11 14:20:01 +0800

    chore: Update lib description

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with io.open(VERSION, encoding='utf-8') as f:
 
 setup(name='ckb_toolkit',
       version=version,
-      description='Nervos CKB Tools',
+      description='Nervos CKB Toolkit',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/duanyytop/ckb-python-toolkit',
